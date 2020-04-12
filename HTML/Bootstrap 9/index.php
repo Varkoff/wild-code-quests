07635ea12ab23ffc282a41a330c388c805882931
Varkoff <virgileboris96@hotmail.fr>
2020-04-12 13:27:53 +0200

    did a quest

---
+++ HTML/Bootstrap 9/index.php
@@ -0,0 +1,207 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <style>
+
+        .col {
+            border: 0.01em solid black;
+            padding: 15px;
+            margin: 1px;
+        }
+
+        .square:before {
+            content: "";
+            display: block;
+            padding-top: 100%; /* initial ratio of 1:1*/
+        }
+    </style>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Bootstrap 9</title>
+</head>
+<body>
+<h1>Bootstrap 9</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+
+        <div class="w-100"></div>
+
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #e58845"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #FFCC00"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #e58845"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+
+        <div class="w-100"></div>
+
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #e58845"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #FFCC00"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: black"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #FFCC00"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #e58845"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+
+        <div class="w-100"></div>
+
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #e58845"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #e58845"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #FFCC00"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: black"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #FFCC00"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #e58845"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #e58845"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+
+        <div class="w-100"></div>
+
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #e58845"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #FFCC00"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: black"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #FFCC00"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #e58845"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+
+        <div class="w-100"></div>
+
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #e58845"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #FFCC00"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #e58845"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+
+        <div class="w-100"></div>
+
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+
+        <div class="w-100"></div>
+
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+
+        <div class="w-100"></div>
+
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+
+        <div class="w-100"></div>
+
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+
+        <div class="w-100"></div>
+
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: #333"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+        <div class="col col-sm-1 col-md-1 col-lg-1 col-xl-1 square" style="background-color: white"></div>
+
+        <div class="w-100"></div>
+
+
+    </div>
+</div>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+</body>
+</html>
+
+
+<?php
+
