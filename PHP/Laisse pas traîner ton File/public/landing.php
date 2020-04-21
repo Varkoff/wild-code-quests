7ee74dc8f81f4df853cb1f34c814021da252dca0
Varkoff <virgileboris96@hotmail.fr>
2020-04-21 12:53:01 +0200

    adding quest

--- PHP/Laisse pas traîner ton File/public/landing.php
+++ PHP/Laisse pas traîner ton File/public/landing.php
@@ -6,89 +6,104 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <title>Landing</title>
+
+    <style>
+        img{
+            width:50%;
+            height: auto;
+            margin: 0 auto;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
+<div class="container">
 <h1>Files uploaded :</h1>
 <?php
-if(isset($_FILES['avatar'])) {
-    $errors     = array();
-    $maxsize    = 1048576;
+
+if (isset($_FILES['avatar'])) {
+    $errors = array();
+    $maxsize = 1000000;
     $acceptable = array(
         'image/jpg',
         'image/gif',
-        'image/png'
+        'image/png',
+        'image/jpeg',
     );
 
-    if(($_FILES['avatar']['size'] >= $maxsize) || ($_FILES["avatar"]["size"] == 0)) {
-        $errors[] = 'File '.'\''.$_FILES['avatar']['name'].'\' = ' .$_FILES['avatar']['size'].'. It is too large. File must be less than 1 megabyte.';
+    $files = [];
+
+    $i = 0;
+    foreach ($_FILES['avatar']['name'] as $key) {
+        array_push($files, array('name' => $_FILES[avatar][name][$i], 'size' => $_FILES[avatar][size][$i], 'type' => $_FILES[avatar][type][$i], 'tmp_name' => $_FILES[avatar][tmp_name][$i], 'error' => $_FILES[avatar][error][$i]));
+        $i++;
     }
 
-    if((!in_array($_FILES['avatar']['type'], $acceptable)) && (!empty($_FILES["avatar"]["type"]))) {
-        $errors[] = 'Invalid file type '.$_FILES['avatar']['type'].' Only wJPG, GIF and PNG types are accepted.';
-    }
 
-    if(count($errors) === 0) {
-//        move_uploaded_file($_FILES['avatar']['tmpname'], '/store/to/location.file');
-        echo "<p style='color:green;'>déplacement possible</p>";
-    } else {
-        foreach($errors as $error) {
-            echo '<p style="color:red;">'.$error.'</p>';
+    foreach ($files as $key) {
+        echo "<hr>";
+        if ($files['error']){
+            echo "<p><span style='color:red;'>Error uploading</span> file".$key['name'];
+        }
+        if (($key['size'] >= $maxsize) || ($key['size'] == 0)) {
+            $errors[] = '\'' . $key['name'] . '\' <span style="color:dodgerblue;">(' .$key['size'] . ') is too large</span>. File must be less than 1 megabyte.';
+            var_dump($key);
+            echo '<p>\'' . $key['name'] . '\' <span style="color:dodgerblue;">(' .$key['size'] . ') is too large</span>. File must be less than '.$maxsize/1000/1000 .' megabyte.</p>';
         }
 
-        //die(); //Ensure no more processing is done
+
+        if ((!in_array($key['type'], $acceptable)) && (!empty($key['type']))) {
+            $errors[] = $key['name']. ' has invalid file type <span style="color:red;">' . $key['type'] . '</span> Only JPG, GIF and PNG types are accepted.';
+            echo "<p>".$key['name']. ' has invalid file type <span style="color:red;">' . $key['type'] . '</span> Only JPG, GIF and PNG types are accepted.</p>';
+        }
+
+        if (count($errors) === 0) {
+            // get only the extension (without the dot)
+            $extension = pathinfo($key['name'], PATHINFO_EXTENSION);
+
+            //A unique name is concatenated with a dot and the $extention avec l'extension récupérée
+            $filename = uniqid() . '.' . $extension;
+
+            // chemin vers un dossier sur le serveur qui va recevoir les fichiers uploadés (attention ce dossier doit être accessible en écriture)
+            $uploadDir = 'uploads/';
+
+            // le nom de fichier sur le serveur est ici généré à partir du nom de fichier sur le poste du client (mais d'autre stratégies de nommage sont possibles)
+            $uploadFile = $uploadDir . $filename;
+
+            // on déplace le fichier temporaire vers le nouvel emplacement sur le serveur. Ca y est, le fichier est uploadé
+            move_uploaded_file($key['tmp_name'], $uploadFile);
+
+            echo "<p style='color:green;'>image ". $key['name']." mise en ligne !</p>";
+            echo '<img src="'.$uploadFile.'">';
+        } else {
+//            foreach ($errors as $error) {
+//                echo '<p>' . $error . '</p>';
+//            }
+            //die(); //Ensure no more processing is done
+        }
     }
+}else{
+    echo "<h4>Array \$_FILES is undefined</h4>";
 }
-
-var_dump($_FILES);
-
-
-
-
-
-
-
-
-
-
-
-//echo '\''.$_FILES['avatar']['name'].'\'';
-
-//foreach ($_FILES['avatar']['name'] as $key){
-//    echo "$key<br>";
-//}
-//$fileFormat = mime_content_type($_FILES['avatar']['tmp_name']);
-//if ( (mime_content_type($_FILES['avatar']['tmp_name'])) == 'image/gif' || (mime_content_type($_FILES['avatar']['tmp_name'])) == 'image/png' || (mime_content_type($_FILES['avatar']['tmp_name'])) =='image/jpeg' || (mime_content_type($_FILES['avatar']['tmp_name'])) == 'image/bmp' || (mime_content_type($_FILES['avatar']['tmp_name'])) =='image/webp'){
-//    echo"<h3>C'est un fichier image ! : $fileFormat\n";
-//}else{
-//    echo"<h3>Ce n'est pas un fichier image : $fileFormat\n";
-//}
-//
-//
-//
-//// get only the extension (without the dot)
-//$extension = pathinfo($_FILES['avatar']['name'], PATHINFO_EXTENSION);
-//
-//echo "$extension\n";
-//// A unique name is concatenated with a dot and the $extention avec l'extension récupérée
-//$filename = uniqid() . '.' .$extension;
-//echo "$filename\n";
-//
-//// chemin vers un dossier sur le serveur qui va recevoir les fichiers uploadés (attention ce dossier doit être accessible en écriture)
-//$uploadDir = 'uploads/';
-//// le nom de fichier sur le serveur est ici généré à partir du nom de fichier sur le poste du client (mais d'autre stratégies de nommage sont possibles)
-//$uploadFile = $uploadDir . $filename;//basename($_FILES['avatar']['name']);
-//
-//// on déplace le fichier temporaire vers le nouvel emplacement sur le serveur. Ca y est, le fichier est uploadé
-//move_uploaded_file($_FILES['avatar']['tmp_name'], $uploadFile);
 ?>
-<a href="upload.php"><hr>Retour</a>
+<hr>
+<a href="upload.php">Retour</a>
+</div>
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+
 </body>
 </html>
--- PHP/Laisse pas traîner ton File/public/upload.php
+++ PHP/Laisse pas traîner ton File/public/upload.php
@@ -8,31 +8,58 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
     <title>Hello, world!</title>
+    <style>
+        img{
+            width:50%;
+            height: auto;
+            margin: 0 auto;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
 <div class="container">
+
     <form action="landing.php" method="post" enctype="multipart/form-data">
         <div class="form-group">
             <label for="imageUpload">Upload a profile image</label>
-            <input type="file" name="avatar[]" multiple="multiple" id="imageUpload"  required/>
+            <input type="file" id="upload_file" name="avatar[]" multiple="multiple" id="imageUpload" required/>
         </div>
         <div class="form-group">
-            <input type="submit">Send</input>
+            <input type="submit" value="Upload images"/>
         </div>
     </form>
-</div>
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
+    <p>Uploaded images</p>
+        <?php
+        $it = new FilesystemIterator(dirname('uploads/.'));
+
+        echo"<div class='row'>";
+        $iteration = 0;
+        foreach ($it as $fileinfo) {
+            $pathname = $fileinfo->getPathname();
+
+            echo "<div class='col-4'>";
+            echo "<img src='$pathname'>";
+            echo "<figcaption>$pathname</figcaption>";
+            echo"</div>";
+            $iteration++;
+            if (($iteration % 3 == 0) && ($iteration>0)){
+                echo "<div class='row'>";
+            }
+        }
+        ?>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+            crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+            crossorigin="anonymous"></script>
 </body>
 </html>
