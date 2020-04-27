9585dc8b9674cfbab2dfc50b9bad37b676663428
Varkoff <virgileboris96@hotmail.fr>
2020-04-27 09:46:35 +0200

    Ajout d'une quête

---
+++ Javascript/Débuter/script.js
@@ -0,0 +1,9 @@
+
+// Define some variables
+const movieName = 'Spider Man';
+const releaseDate = 2002;
+const movieDirector = "Sam Raimi";
+const message = 'My favorite movie is ' + movieName + '('+releaseDate+') by ' + movieDirector + '!';
+
+// Display a popup
+alert(message);
